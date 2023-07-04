b0c4b2ef6fcf54a47f4b57a3905e08827d112943
Zhang-Liao <zhangliao714@gmail.com>
2023-07-04 17:00:48 +0200

    separate

---
+++ prolog/examples/grandparent/sparate/bk.pl
@@ -0,0 +1,54 @@
+:- use_module(library(aleph)).
+:- if(current_predicate(use_rendering/1)).
+:- use_rendering(prolog).
+:- endif.
+:- aleph.
+:- modeh(*,grandparent(+person,-person)).
+
+:- modeb(*,mother(+person,-person)).
+:- modeb(*,father(+person,-person)).
+
+:- determination(grandparent/2,father/2).
+:- determination(grandparent/2,parent/2).
+:- determination(grandparent/2,mother/2).
+
+:-dynamic grandparent/2.
+:-begin_bg.
+person(bob).
+person(dad(bob)).
+person(mum(bob)).
+person(dad(dad(bob))).
+person(dad(mum(bob))).
+person(mum(dad(bob))).
+person(mum(mum(bob))).
+
+person(jo).
+person(dad(jo)).
+person(mum(jo)).
+person(dad(dad(jo))).
+person(dad(mum(jo))).
+person(mum(dad(jo))).
+person(mum(mum(jo))).
+
+person(peter).
+person(dad(peter)).
+person(mum(peter)).
+person(dad(dad(peter))).
+person(dad(mum(peter))).
+person(mum(dad(peter))).
+person(mum(mum(peter))).
+
+person(jane).
+person(dad(jane)).
+person(mum(jane)).
+person(dad(dad(jane))).
+person(dad(mum(jane))).
+person(mum(dad(jane))).
+person(mum(mum(jane))).
+
+father(dad(X),X):-
+   person(X).
+mother(mum(X),X):-
+   person(X).
+
+:-end_bg.
